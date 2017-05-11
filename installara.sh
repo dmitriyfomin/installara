547e0fe427566eeb2ee7aad5961695f5f37d72d7
Dmitry Fomin <dfgit@ya.ru>
2017-05-11 04:32:50 +0000

    commit
    Upgrade

--- installara.sh
+++ installara.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #*********************************#
 #           INSTALLARA            #
 #                                 #
@@ -11,50 +11,60 @@
 # Set colour vars
 white='\e[1;37m'
 red='\e[1;31m'
-orange='\e[0;29m'
+orange='\e[38;5;209m'
 cyan='\e[0;36m'
 purple='\e[0;35m'
 yellow='\e[0;33m'
+spec='\e[38;5;203m'
 
 # Menu (Title screen)
 clear
 echo
-echo "                     $white #*********************************#"
-echo "                     $white #                                 #"
-echo "                     $white #           $red INSTALLARA           $white#"
-echo "                     $white #                                 #"
-echo "                     $white #     $orange Author: Dmitry Fomin       $white#"
-echo "                     $white #                                 #"
-echo "                     $white #*********************************#"
-echo ""
-echo "                                      $purple OPTIONS:"
+echo -e $white"                      #*********************************#"
+echo -e $white"                      #                                 #"
+echo -e $white"                      #           $red INSTALLARA $white          #"
+echo -e $white"                      #                                 #"
+echo -e $white"                      #     $orange Author: Dmitry Fomin       $white#"
+echo -e $white"                      #                                 #"
+echo -e $white"                      #*********************************#"
 echo
-echo "                      $yellow 1)$white START INSTALLING"
-echo "                      $yellow 2)$white QUIT"
+echo -e $purple"                                       OPTIONS:"
+echo
+echo -e "                      $yellow 1)$white START INSTALLING"
+echo -e "                      $yellow 2)$white QUIT"
 
 # Options
+echo
+echo -ne $spec"installara> $white"
 read option
 if [ $option = 1 ];
  then
    echo
-   echo "$cyan Step 1. Enter name of your project or domain name ([a-z0-9-.a-z]): $white"
+   echo -e $cyan"Step 1. Enter name of your project or domain name ([a-z0-9-.a-z]): $white"
+   echo
+   echo -ne $spec"installara> $white"
    read projectname
    if [ ! -z $projectname ];
     then
-      echo "$orange Step 2. Enter Laravel Framework version ([~5.*0-9.*0-9]) to install it: $white"
-      read ver
-      echo "$white ********* STARTING INSTALL... ***********"
       echo
-      echo "$purple Project folder: $white $(pwd)/$projectname,$red Laravel:$white $ver"
+      echo -e $orange"Step 2. Enter Laravel Framework version ([~5.*0-9.*0-9]) to install it: $white"
+      echo
+      echo -ne $spec"installara> $white"
+      read ver
+      echo -e "$white ********* STARTING INSTALL... ***********"
+      echo
+      echo -e "$purple Project folder: $white $(pwd)/$projectname,$red Laravel:$white $ver"
       echo
       curl -sS https://getcomposer.org/installer | php
       php composer.phar create-project laravel/laravel $projectname "$ver"
-      chmod -R 775 $projectname/storage/
+      chmod -R 775 $projectname/storage/ && zenity --info --text "OK! Thanks for using Installara" && clear && exit
    else
-    echo "$red Error! Empty name"
+    echo -e $red"Error! Empty name"
     sleep 0.9
-    sh $(basename $0)
+    bash $(basename $0)
   fi
 else
+   zenity --info --text "Thanks for using Installara"
+   clear
    exit
 fi
